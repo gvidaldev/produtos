9e5ec92e7bca697961d31146eb226793622ffa04
Gabriel <gvidal.dev@gmail.com>
2019-12-19 02:35:40 -0300

    Initial Commit. Initial structure

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <div>
+      <nav className='navbar navbar-inverse'>
+        <div className='container'>
+          <div className='navbar-header'>
+            <a href='/' className='navbar-brand'>
+              Gerenciador de Produtos
+            </a>
+          </div>
+          <ul className='nav navbar-nav'>
+            <li><a href='/'>Home</a></li>
+            <li><a href='/'>Produtos</a></li>
+            <li><a href='/'>Sobre</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className='container'>
+        <h1>Gerenciador de Produtos</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App

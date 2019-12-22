4ca3e409f1843cab08cc860bd8a5b746be16d056
Gabriel Vidal <gvidal.dev@gmail.com>
2019-12-22 18:44:45 -0300

    loading server data

--- src/Components/Produtos.js
+++ src/Components/Produtos.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import axios from 'axios'
+
 import ProdutosHome from './ProdutosHome'
 import Categoria from './Categoria'
 
 class Produtos extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            categorias: []
+        }
+    }
+    componentDidMount(){
+        axios
+            .get('http://localhost:3001/categorias')
+            .then(res => {
+                this.setState({
+                    categorias: res.data
+                })
+            })
+    }
+    renderCategoria(cat){
+        return (
+            <li key={cat.id}>
+                <Link to={`/produtos/categoria/${cat.id}`}>{cat.categoria}</Link>
+            </li>
+        )
+    }
     render(){
         const {match} = this.props
+        const { categorias } = this.state 
         return(
             <div className='container'>
                 <div className='row'>
                     <div className='col-sm-2'>
                         <h3>Categorias</h3>
-                        <Link to='/produtos/categoria/1'>Categoria 1</Link>
+                        <ul>
+                            {categorias.map(this.renderCategoria)}
+                        </ul>
+    
                     </div>
                 
                     <div className='col-sm-10'>
@@ -21,7 +49,7 @@ class Produtos extends Component{
                     </div>
                 </div>
             </div>
-        )   
+        )    
 
     }
 }
